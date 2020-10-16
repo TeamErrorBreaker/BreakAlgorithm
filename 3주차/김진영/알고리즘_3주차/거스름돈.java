f79dc82a58da1ade4e3d113e23ca54995f261475
jeeelung <jeeelung2@gmail.com>
2020-10-16 21:11:21 +0900

    거스름돈

--- 3주차/김진영/알고리즘_3주차/거스름돈.java
+++ 3주차/김진영/알고리즘_3주차/거스름돈.java
@@ -1,8 +1,48 @@
 package 김진영.알고리즘_3주차;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 거스름돈 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int price = Integer.parseInt(br.readLine());
+		int target = 1000 - price;
 		
+		int count = 0;
+		
+		while(target>=500) {
+			target = target-500;
+			count++;
+		}
+		
+		while(target>=100) {
+			target = target-100;
+			count++;			
+		}
+		
+		while(target>=50) {
+			target = target-50;
+			count++;	
+		}
+		
+		while(target>=10) {
+			target = target-10;
+			count++;			
+		}
+		
+		while(target>=5) {
+			target = target-5;
+			count++;			
+		}
+		
+		while(target!=0) {
+			target = target-1;
+			count++;			
+		}
+		
+		System.out.println(count);
 	}
 }
