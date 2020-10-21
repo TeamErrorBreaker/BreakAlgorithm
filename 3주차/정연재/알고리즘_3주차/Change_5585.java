893c006bebde1dae9c81ebd3a08e6ff991191c32
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2020-10-21 15:48:59 +0900

    50원짜리를 빠뜨렸었다..ㅎ

--- 3주차/정연재/알고리즘_3주차/Change_5585.java
+++ 3주차/정연재/알고리즘_3주차/Change_5585.java
@@ -1,25 +1,21 @@
 package 정연재.알고리즘_3주차;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Change_5585 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// 거스름돈 알고리즘
-		Scanner sc = new Scanner(System.in);
-
-		int max = 1000;//최댓값
-		int min = 1;//최소값
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int pay = sc.nextInt();
-		int coin[] = {500,100,10,5,1};
+		int pay = Integer.parseInt(br.readLine());
+		int coin[] = {500,100,50,10,5,1};
 		int count = 0;
 		int change = 1000 - pay;
-		if(pay > min && pay < max) {
-			for(int i=0;i<coin.length;i++) {
-				count += change / coin[i];
-				change = change % coin[i];
-			}
+		for(int i=0;i<coin.length;i++) {
+			count += change / coin[i];
+			change = change % coin[i];
 		}
 		System.out.println(count);
 	}
