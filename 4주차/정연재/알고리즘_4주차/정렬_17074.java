afd66890bba6a28009cf6832f68b978dbe953b9a
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2020-10-30 03:52:50 +0900

    풀이

--- 4주차/정연재/알고리즘_4주차/정렬_17074.java
+++ 4주차/정연재/알고리즘_4주차/정렬_17074.java
@@ -1,5 +1,13 @@
 package 정연재.알고리즘_4주차;
 
-public class 정렬_17074 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class 정렬_17074 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(br.readLine());
+		
+	}
 }
