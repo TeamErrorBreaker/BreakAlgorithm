57dd342f8a1e677c61606a53d77aea39031acce8
d0 <dip.0cean@daum.net>
2020-10-30 03:36:31 +0900

    알고리즘 4주차 / 바이러스 , 정렬

--- 4주차/심성헌/알고리즘_4주차/바이러스_2606.java
+++ 4주차/심성헌/알고리즘_4주차/바이러스_2606.java
@@ -1,21 +1,46 @@
 package 심성헌.알고리즘_4주차;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 바이러스_2606 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int n;
 	static int m;
+	static int count;
+	static boolean[][] graph;
+	static boolean[] visited;
 
-	static void dfs(int x, int y) {
-		Stack<Integer> stack = new Stack<Integer>();
+	public static void dfs(int index) {
+		visited[index] = true;
 
+		for (int i = 1; i <= n; i++) {
+			if (graph[index][i] == true && visited[i] == false) {
+				count++;
+				dfs(i);
+			}
+		}
 	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());		
+		m = Integer.parseInt(br.readLine());
+
+		graph = new boolean[n + 1][n + 1];
+		visited = new boolean[n + 1];
+
+		for (int i = 1; i <= m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			graph[x][y] = true;
+			graph[y][x] = true;
+		}
+		dfs(1);
+		System.out.println(count);
 	}
 }
---
+++ 4주차/심성헌/알고리즘_4주차/정렬_17074.java
@@ -0,0 +1,61 @@
+package 심성헌.알고리즘_4주차;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 정렬_17074 {
+// https://data-bank.tistory.com/4
+// 내 코드는 주어진 배열에서 하나씩 빼면서 정렬인지 아닌지 확인하기 때문에 시간 초과 문제 발생
+// 위의 블로그를 참고해서 경우의 수를 제거하는 방법으로 코드를 짜보자!
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int n;
+	static int[] m;
+	static int result;
+
+	public static void main(String[] args) throws Exception {
+		n = Integer.parseInt(br.readLine());
+		m = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = 0;
+		while (st.hasMoreTokens()) {
+			m[count] = Integer.parseInt(st.nextToken());
+			count++;
+		}
+
+		factorial(0);
+		System.out.println(result);
+	}
+
+	public static void factorial(int index) {
+		boolean check = true;
+		int[] arr = new int[n - 1];
+		int count = 0;
+		if (index >= m.length)
+			return;
+		for (int i = 0; i < n; i++) {
+			if (i == index) {
+				continue;
+			} else {
+				arr[count] = m[i];
+				System.out.print(arr[count]);
+				count++;
+			}
+		}
+		System.out.println();
+		int save = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (save <= arr[i]) {
+				save = arr[i];
+				check = true;
+			} else {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			result++;
+		}
+		factorial(index + 1);
+	}
+}
